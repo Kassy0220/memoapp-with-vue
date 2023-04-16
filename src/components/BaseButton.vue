bbc8ad32c43a26d488e176bcbf06c09e9c3e160c
github Kassy <github@shinbokutaro0220@gmail.com>
2023-04-17 07:42:46 +0900

    共通ボタンにCSSを追加

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -7,3 +7,15 @@ export default {
 <template>
   <button @click="$emit('click')"><slot></slot></button>
 </template>
+
+<style scoped>
+button {
+  padding: 0.2em 0.5em 0.2em 0.5em;
+  border: solid 1px #555555;
+  border-radius: 3px;
+}
+button:hover {
+  color: white;
+  background-color: #555555;
+}
+</style>
