422c43067a4928478f99fd12f51dc99327bd9dd1
github Kassy <github@shinbokutaro0220@gmail.com>
2023-04-19 08:34:37 +0900

    メモ編集機能を追加

---
+++ src/components/MemoEditForm.vue
@@ -0,0 +1,26 @@
+<script>
+import BaseButton from './BaseButton.vue'
+
+export default {
+  components: {
+    BaseButton
+  },
+  props: {
+    id: String,
+    content: String
+  },
+  emits: ['update:content', 'updateMemo']
+}
+</script>
+
+<template>
+  <div>
+    <form id="update-form">
+      <textarea
+        :value="content"
+        @input="$emit('update:content', $event.target.value)"
+      />
+      <BaseButton @click="$emit('updateMemo', '#update-form', id, content)">更新</BaseButton>
+    </form>
+  </div>
+</template>
--- src/components/MemoList.vue
+++ src/components/MemoList.vue
@@ -1,9 +1,11 @@
 <script>
 import BaseButton from './BaseButton.vue'
+import MemoEditForm from './MemoEditForm.vue'
 
 export default {
   components: {
-    BaseButton
+    BaseButton,
+    MemoEditForm
   },
   data() {
     return {
@@ -29,6 +31,35 @@ export default {
     deleteMemo(id) {
       this.allMemos = this.allMemos.filter((memo) => !(memo.id === id))
       localStorage.removeItem(id)
+    },
+    editMemo(id) {
+      if (this.allMemos.find((memo) => memo.isEditing)) {
+        alert('既に編集中のメモがあります')
+        return
+      }
+
+      for (const memo of this.allMemos) {
+        if (memo.id === id) {
+          memo.isEditing = true
+        }
+      }
+    },
+    updateMemo(formElement, id, content) {
+      if (!content) {
+        alert('メモの内容を入力してください')
+        return
+      }
+
+      let updateForm = this.$el.querySelector(formElement)
+      updateForm.submit()
+
+      for (const memo of this.allMemos) {
+        if (memo.id === id) {
+          memo.content = content
+          memo.isEditing = false
+        }
+      }
+      localStorage.setItem(id, content)
     }
   }
 }
@@ -42,8 +73,19 @@ export default {
         v-for="memo in allMemos"
         :key="memo.id"
       >
-        {{ memo.content }}
-        <BaseButton @click="deleteMemo(memo.id)">削除</BaseButton>
+        <template v-if="memo.isEditing">
+          <MemoEditForm
+            :id="memo.id"
+            v-model:content="memo.content"
+            @updateMemo="updateMemo"
+          ></MemoEditForm>
+        </template>
+
+        <template v-else>
+          {{ memo.content }}
+          <BaseButton @click="editMemo(memo.id)">編集</BaseButton>
+          <BaseButton @click="deleteMemo(memo.id)">削除</BaseButton>
+        </template>
       </li>
     </ul>
   </div>
